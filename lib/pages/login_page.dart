c509f7ebeb0a1c6f6fa43cdde27958e4d535dee9
Emery-hub <emerychikezie3@gmail.com>
2021-09-02 03:53:53 +0100

    Chapter three completed

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,99 @@
+import 'package:awesome_app/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key? key}) : super(key: key);
+
+  static const String routeName = "/login";
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+
+  final _usernameController = TextEditingController();
+
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login Page"),
+      ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            "assets/bg.jpeg",
+            fit: BoxFit.cover,
+            color: Colors.black.withOpacity(0.7),
+            colorBlendMode: BlendMode.darken,
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SingleChildScrollView(
+                child: Form(
+                  key: formKey,
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            controller: _usernameController,
+                            keyboardType: TextInputType.emailAddress,
+                            validator: (s) {},
+                            decoration: InputDecoration(
+                              hintText: "Enter Your Email",
+                              labelText: "Username",
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            controller: _passwordController,
+                            keyboardType: TextInputType.text,
+                            validator: (s) {},
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintText: "Enter Your Passwrord",
+                              labelText: "Password",
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          RaisedButton(
+                            onPressed: () {
+                              // formKey.currentState!.validate();
+
+                              // Navigator.push(
+                              //   context,
+                              //   MaterialPageRoute(
+                              //     builder: (context) => HomePage(),
+                              //   ),
+                              // );
+
+                              Navigator.pushNamed(context, HomePage.routeName);
+                            },
+                            child: Text("Sign In"),
+                            color: Colors.orangeAccent,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
